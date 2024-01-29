c4be44b0e11d7c174c6cff937a5f9a962bef7ba9
HenriqueCortazzo <qdeusmeajude@gmail.com>
2024-01-29 05:09:32 -0300

    Finalização de Exercício

--- Resolution.java
+++ Resolution.java
@@ -5,13 +5,14 @@ public class Resolution {
         Random random = new Random();
         int[] array1 = new int[10];
         int[] array2 = new int[10];
+        int menor = Integer.MAX_VALUE;
         int[] menorValorCadaArray = new int[10];
-        int menor = 100;
-        int menorMenores = 100;
+        int menorMenores = Integer.MAX_VALUE;
+        int valor1 = 0,valor2 = 0;
         int [] distancia = new int[10];
         for (int i = 0; i < 10; i++) {
-            array1[i] = random.nextInt(10) + 1;;
-            array2[i] = random.nextInt(20) + 5;
+            array1[i] = random.nextInt(10);
+            array2[i] = random.nextInt(40) + 10;
         }
         for (int s: array1){
             System.out.print(s+" ");
@@ -20,12 +21,13 @@ public class Resolution {
         for (int s: array2){
             System.out.print(s+" ");
         }
-
         for (int i = 0; i < array1.length; i++) {
             for (int j = 0; j < array2.length; j++) {
-                distancia[j] = array2[i] - array1[j];
+                distancia[j] = Math.abs(array1[i] - array2[j]);
                 if (menor>distancia[j]){
                     menor = distancia[j];
+                    valor1 = array1[i];
+                    valor2 =array2[j];
                 }
                 menorValorCadaArray[i] = menor;
                 if (menorMenores>menorValorCadaArray[i]){
@@ -34,7 +36,6 @@ public class Resolution {
             }
         }
         System.out.println();
-        System.out.println(menorMenores);
-
+        System.out.printf("A menor distância é %d entre %d e %d",menorMenores,valor2,valor1);
     }
 }
